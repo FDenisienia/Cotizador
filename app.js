1920025818f69dd7209ace2ea0cf046eee19fc07
FDenisienia <federicodenisienia1@gmail.com>
2022-07-19 21:20:39 -0300

    commit

--- app.js
+++ app.js
@@ -3,10 +3,17 @@ const pesoArg = document.getElementById('number');
 const button = document.getElementById('submit')
 const final = document.getElementById('final')
 
+let precioUSD;
+
+fetch('https://api.bluelytics.com.ar/v2/latest')
+  .then(res => res.json())
+  .then(data => precioUSD = data.blue.value_avg)
+
+
 const calcularPesoAUsd = (event) => {
     event.preventDefault()
-    calculo = pesoArg.value / USD
-    final.innerHTML = `${pesoArg.value} ARG =  ${calculo} USD`;
+    calculo = pesoArg.value / precioUSD
+    final.innerHTML = `${pesoArg.value} ARG =  ${calculo.toFixed(2)} USD`;
 }
 
 button.addEventListener('click', calcularPesoAUsd)
